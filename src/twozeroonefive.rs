7c10c5c91a480b0b8143d14789f2496f8048557f
mdeg <michael@dega.no>
2021-12-19 12:33:36 +1100

    2015 Day 2

--- src/twozeroonefive.rs
+++ src/twozeroonefive.rs
@@ -1,5 +1,9 @@
 mod day1;
+mod day2;
+mod day3;
 
 pub fn run() {
-    day1::run()
+    //day1::run()
+    //day2::run()
+    day3::run()
 }
---
+++ src/twozeroonefive/day3.rs
@@ -0,0 +1,35 @@
+use std::cmp::min;
+
+pub fn run() {
+    let input: Vec<Vec<i32>> = include_str!("day2input")
+        .lines()
+        .map(|l| {
+            let test = l.split("x").map(|i| i.parse::<i32>().unwrap()).collect();
+            println!("{:?}", test);
+            test
+        }).collect();
+
+    println!("{:?}", input);
+
+    pt1(&input);
+    pt2(&input);
+}
+
+fn pt1(input: &Vec<Vec<i32>>) {
+    let result: i32 = input.iter().map(|set| {
+        let (l, w, h) = (set[0], set[1], set[2]);
+        (2 * (l * w)) + (2 * (w * h)) + (2 * (h * l)) + min(min((l * w), (w * h)), (h * l))
+    }).sum();
+    println!("pt1: {}", result)
+}
+
+fn pt2(input: &Vec<Vec<i32>>) {
+    let result: i32 = input.iter().map(|set| {
+        let mut set = set.clone();
+        set.sort();
+        let ribbon = (set[0] * 2) + (set[1] * 2);
+        let bow = (set[0] * set[1] * set[2]);
+        ribbon + bow
+    }).sum();
+    println!("pt1: {}", result)
+}
